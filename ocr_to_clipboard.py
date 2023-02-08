5653bd5da7eb02f65ea906625bcc21d1a75cc786
Jon <jbvance_imap@hotmail.com>
2023-02-08 14:52:55 -0600

    something

--- ocr_to_clipboard.py
+++ ocr_to_clipboard.py
@@ -5,33 +5,29 @@ import pytesseract
 import datetime
 import clipboard
 
-# (1) Wait for user to trigger an image capture
-input("Press Enter to capture image...")
+while True:
+    # (1) Wait for user to trigger an image capture
+    input("\nPress Enter to capture image...")
 
-# (2) Capture image from USB camera
-print("starting camera = cv2.VideoCapture(0)"); 
-camera = cv2.VideoCapture(0)
-print("starting image = camera.read()") 
-return_value, image = camera.read()
-print("starting camera.release()")
-camera.release()
+    # (2) Capture image from USB camera
+    camera = cv2.VideoCapture(0)
+    return_value, image = camera.read()
+    camera.release()
 
-# (3) Save image to images subfolder with time-date stamp on file name
-print("starting timestamp = datetime.datetime.now().strftime(...)")
-timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-print(timestamp)
-print("starting file_name = ""images/captured_image_"" + timestamp + "".png""")
-file_name = "images/captured_image_" + timestamp + ".png"
-print(file_name)
-print("starting cv2.imwrite(file_name, image)")
-cv2.imwrite(file_name, image)
+    # (3) Save image to images subfolder with time-date stamp on file name
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = "images/captured_image_" + timestamp + ".png"
+    print("File name:  " + file_name)
+    cv2.imwrite(file_name, image)
 
-# (4) Process image with Pytesseract for OCR string
-print("starting text = pytesseract.image_to_string(image)")
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-text = pytesseract.image_to_string(image)
+    # (4) Process image with Pytesseract for OCR string
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(image)
 
-# (5) Send string to clipboard
-clipboard.copy(text)
-print(text)
-print("OCR text copied to clipboard.")
+    # (5) Send string to clipboard
+    clipboard.copy(text)
+    print(text)
+    print("OCR text copied to clipboard.")
+
+    cv2.imshow('image',image)
+    cv2.waitKey(0)
